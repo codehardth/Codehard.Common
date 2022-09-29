ba10dff9ed2e9fb0c1ca55a4efa13b822a6c1afc
Rungwiroon Komalitipong <rungwiroon@gmail.com>
2022-09-30 01:00:46 +0700

    Log, Fin<A> and Aff<A> extensions refactor

--- Codehard.Functional.AspNetCore/GuardExtensions.cs
+++ Codehard.Functional.AspNetCore/GuardExtensions.cs
@@ -2,87 +2,87 @@ namespace Codehard.Functional.AspNetCore;
 
 public static class GuardExtensions
 {
-    public static Guard<LanguageExt.Common.Error> GuardWithOK(bool flag, string message = "")
+    public static Guard<Error> GuardWithOK(bool flag, string message = "")
     {
-        return new Guard<LanguageExt.Common.Error>(
+        return new Guard<Error>(
             flag,
-            LanguageExt.Common.Error.New((int) HttpStatusCode.OK, message));
+            Error.New((int) HttpStatusCode.OK, message));
     }
 
-    public static Guard<LanguageExt.Common.Error> GuardWithCreated(bool flag, string message = "")
+    public static Guard<Error> GuardWithCreated(bool flag, string message = "")
     {
-        return new Guard<LanguageExt.Common.Error>(
+        return new Guard<Error>(
             flag,
-            LanguageExt.Common.Error.New((int) HttpStatusCode.Created, message));
+            Error.New((int) HttpStatusCode.Created, message));
     }
 
-    public static Guard<LanguageExt.Common.Error> GuardWithAccepted(bool flag, string message = "")
+    public static Guard<Error> GuardWithAccepted(bool flag, string message = "")
     {
-        return new Guard<LanguageExt.Common.Error>(
+        return new Guard<Error>(
             flag,
-            LanguageExt.Common.Error.New((int) HttpStatusCode.Accepted, message));
+            Error.New((int) HttpStatusCode.Accepted, message));
     }
 
-    public static Guard<LanguageExt.Common.Error> GuardWithNoContent(bool flag, string message = "")
+    public static Guard<Error> GuardWithNoContent(bool flag, string message = "")
     {
-        return new Guard<LanguageExt.Common.Error>(
+        return new Guard<Error>(
             flag,
-            LanguageExt.Common.Error.New((int) HttpStatusCode.NoContent, message));
+            Error.New((int) HttpStatusCode.NoContent, message));
     }
 
-    public static Guard<LanguageExt.Common.Error> GuardWithBadRequest(bool flag, string message = "")
+    public static Guard<Error> GuardWithBadRequest(bool flag, string message = "")
     {
-        return new Guard<LanguageExt.Common.Error>(
+        return new Guard<Error>(
             flag,
-            LanguageExt.Common.Error.New((int) HttpStatusCode.BadRequest, message));
+            Error.New((int) HttpStatusCode.BadRequest, message));
     }
 
-    public static Guard<LanguageExt.Common.Error> GuardWithUnauthorized(bool flag, string message = "")
+    public static Guard<Error> GuardWithUnauthorized(bool flag, string message = "")
     {
-        return new Guard<LanguageExt.Common.Error>(
+        return new Guard<Error>(
             flag,
-            LanguageExt.Common.Error.New((int) HttpStatusCode.Unauthorized, message));
+            Error.New((int) HttpStatusCode.Unauthorized, message));
     }
 
-    public static Guard<LanguageExt.Common.Error> GuardWithForbidden(bool flag, string message = "")
+    public static Guard<Error> GuardWithForbidden(bool flag, string message = "")
     {
-        return new Guard<LanguageExt.Common.Error>(
+        return new Guard<Error>(
             flag,
-            LanguageExt.Common.Error.New((int) HttpStatusCode.Forbidden, message));
+            Error.New((int) HttpStatusCode.Forbidden, message));
     }
 
-    public static Guard<LanguageExt.Common.Error> GuardWithNotFound(bool flag, string message = "")
+    public static Guard<Error> GuardWithNotFound(bool flag, string message = "")
     {
-        return new Guard<LanguageExt.Common.Error>(
+        return new Guard<Error>(
             flag,
-            LanguageExt.Common.Error.New((int) HttpStatusCode.NotFound, message));
+            Error.New((int) HttpStatusCode.NotFound, message));
     }
 
-    public static Guard<LanguageExt.Common.Error> GuardWithConflict(bool flag, string message = "")
+    public static Guard<Error> GuardWithConflict(bool flag, string message = "")
     {
-        return new Guard<LanguageExt.Common.Error>(
+        return new Guard<Error>(
             flag,
-            LanguageExt.Common.Error.New((int) HttpStatusCode.Conflict, message));
+            Error.New((int) HttpStatusCode.Conflict, message));
     }
 
-    public static Guard<LanguageExt.Common.Error> GuardWithUnprocessableEntity(bool flag, string message = "")
+    public static Guard<Error> GuardWithUnprocessableEntity(bool flag, string message = "")
     {
-        return new Guard<LanguageExt.Common.Error>(
+        return new Guard<Error>(
             flag,
-            LanguageExt.Common.Error.New((int) HttpStatusCode.UnprocessableEntity, message));
+            Error.New((int) HttpStatusCode.UnprocessableEntity, message));
     }
 
-    public static Guard<LanguageExt.Common.Error> GuardWithLocked(bool flag, string message = "")
+    public static Guard<Error> GuardWithLocked(bool flag, string message = "")
     {
-        return new Guard<LanguageExt.Common.Error>(
+        return new Guard<Error>(
             flag,
-            LanguageExt.Common.Error.New((int) HttpStatusCode.Locked, message));
+            Error.New((int) HttpStatusCode.Locked, message));
     }
 
-    public static Guard<LanguageExt.Common.Error> GuardWithInternalServerError(bool flag, string message = "")
+    public static Guard<Error> GuardWithInternalServerError(bool flag, string message = "")
     {
-        return new Guard<LanguageExt.Common.Error>(
+        return new Guard<Error>(
             flag,
-            LanguageExt.Common.Error.New((int) HttpStatusCode.InternalServerError, message));
+            Error.New((int) HttpStatusCode.InternalServerError, message));
     }
 }
