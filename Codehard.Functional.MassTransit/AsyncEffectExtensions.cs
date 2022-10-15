aeeb37cff93ff40802fe1c2237fa2347408db699
Rungwiroon Komalitipong <rungwiroon@gmail.com>
2022-10-16 00:27:35 +0700

    Move AffWithConsumeContext to separated file

--- Codehard.Functional.MassTransit/AsyncEffectExtensions.cs
+++ Codehard.Functional.MassTransit/AsyncEffectExtensions.cs
@@ -1,8 +1,5 @@
-﻿using Codehard.Functional.Logger;
-using Codehard.Functional.MassTransit.Interfaces;
-using LanguageExt.Common;
+﻿using Codehard.Functional.MassTransit.CombinedAffTypes;
 using MassTransit;
-using Microsoft.Extensions.Logging;
 using System.Diagnostics.Contracts;
 
 namespace Codehard.Functional.MassTransit;
@@ -17,59 +14,5 @@ public static class AsyncEffectExtensions
         return new AffWithConsumeContext<T>(
             aff,
             consumerContext);
-    }
-}
-
-public struct AffWithConsumeContext<TRes>
-{
-    private readonly Aff<TRes> aff;
-
-    private readonly ConsumeContext consumeContext;
-
-    public AffWithConsumeContext(Aff<TRes> aff, ConsumeContext consumeContext)
-    {
-        this.aff = aff;
-        this.consumeContext = consumeContext;
-    }
-
-    public async ValueTask<Fin<Unit>> RunAndResponseAsync<TSuccResp, TFailResp>(
-        Func<Error, object> errorRespMsgFunc,
-        ILogger? logger = default)
-        where TSuccResp : class, ICorrelatableMessage
-        where TFailResp : class, IFaultMessage
-    {
-        var @this = this;
-
-        var respAff =
-            aff.MatchAff(
-                Succ: res =>
-                    @this.consumeContext.RespondAsync<TSuccResp>(res)
-                         .ToUnit()
-                         .ToAff(),
-                Fail: err =>
-                    @this.consumeContext.RespondAsync<TFailResp>(errorRespMsgFunc(err))
-                         .ToUnit()
-                         .ToAff());
-
-        var respFin = await respAff.Run();
-        logger?.LogIfFail(respFin);
-
-        return respFin;
-    }
-
-    public ValueTask<Fin<Unit>> RunAndResponseAsync<TSuccResp>(
-        ILogger? logger = default)
-        where TSuccResp : class, ICorrelatableMessage
-    {
-        var @this = this;
-        var correlationId = @this.consumeContext.CorrelationId;
-
-        return RunAndResponseAsync<TSuccResp, IFaultMessage>(
-            err => new
-            {
-                Reason = err.Message,
-                CorrelationId = correlationId
-            },
-            logger);
     }
 }
---
+++ Codehard.Functional.MassTransit/CombinedAffTypes/AffWithConsumeContext.cs
@@ -0,0 +1,74 @@
+﻿using Codehard.Functional.Logger;
+using Codehard.Functional.MassTransit.Interfaces;
+using LanguageExt.Common;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+
+namespace Codehard.Functional.MassTransit.CombinedAffTypes
+{
+    /// <summary>
+    /// Aff pair with Masstransit ConsumeContext
+    /// </summary>
+    public readonly struct AffWithConsumeContext<TRes>
+    {
+        private readonly Aff<TRes> aff;
+
+        private readonly ConsumeContext consumeContext;
+
+        /// <summary>
+        /// Construct Aff with ConsumeContext
+        /// </summary>
+        public AffWithConsumeContext(Aff<TRes> aff, ConsumeContext consumeContext)
+        {
+            this.aff = aff;
+            this.consumeContext = consumeContext;
+        }
+
+        /// <summary>
+        /// Invoke the effect and response result to caller
+        /// </summary>
+        public async ValueTask<Fin<Unit>> RunAndResponseAsync<TSuccResp, TFailResp>(
+            Func<Error, object> errorRespMsgFunc,
+            ILogger? logger = default)
+            where TSuccResp : class, ICorrelatableMessage
+            where TFailResp : class, IFaultMessage
+        {
+            var consumeContext = this.consumeContext;
+
+            var respAff =
+                aff.MatchAff(
+                    Succ: res =>
+                        consumeContext.RespondAsync<TSuccResp>(res)
+                             .ToUnit()
+                             .ToAff(),
+                    Fail: err =>
+                        consumeContext.RespondAsync<TFailResp>(errorRespMsgFunc(err))
+                             .ToUnit()
+                             .ToAff());
+
+            var respFin = await respAff.Run();
+            logger?.LogIfFail(respFin);
+
+            return respFin;
+        }
+
+        /// <summary>
+        /// Invoke the effect and response result to caller
+        /// </summary>
+        public ValueTask<Fin<Unit>> RunAndResponseAsync<TSuccResp>(
+            ILogger? logger = default)
+            where TSuccResp : class, ICorrelatableMessage
+        {
+            var @this = this;
+            var correlationId = @this.consumeContext.CorrelationId;
+
+            return RunAndResponseAsync<TSuccResp, IFaultMessage>(
+                err => new
+                {
+                    Reason = err.Message,
+                    CorrelationId = correlationId
+                },
+                logger);
+        }
+    }
+}
