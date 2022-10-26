6e0be9a5c614574a75a156592a06fff3c084803d
Chanvut Booneid <chanvut.b@codehard.co.th>
2022-10-26 16:20:52 +0700

    Convert legacy namespace convention to file scoped namespace

--- Codehard.Functional/Prelude.cs
+++ Codehard.Functional/Prelude.cs
@@ -1,33 +1,32 @@
-﻿namespace Codehard.Functional
+﻿namespace Codehard.Functional;
+
+public static class Prelude
 {
-    public static class Prelude
+    public static Aff<Option<A>> AffOption<A>(Func<Task<A?>> f)
+        where A : class
     {
-        public static Aff<Option<A>> AffOption<A>(Func<Task<A?>> f)
-            where A : class
-        {
-            return LanguageExt.Aff<Option<A>>
-                .Effect(async () => Optional(await f()));
-        }
+        return LanguageExt.Aff<Option<A>>
+            .Effect(async () => Optional(await f()));
+    }
 
-        public static Aff<Option<A>> AffOption<A>(Func<Task<Nullable<A>>> f)
-            where A : struct
-        {
-            return LanguageExt.Aff<Option<A>>
-                .Effect(async () => Optional(await f()));
-        }
+    public static Aff<Option<A>> AffOption<A>(Func<Task<Nullable<A>>> f)
+        where A : struct
+    {
+        return LanguageExt.Aff<Option<A>>
+            .Effect(async () => Optional(await f()));
+    }
 
-        public static Aff<Option<A>> AffOption<A>(Func<ValueTask<A?>> f)
-            where A : class
-        {
-            return LanguageExt.Aff<Option<A>>
-                .Effect(async () => Optional(await f()));
-        }
+    public static Aff<Option<A>> AffOption<A>(Func<ValueTask<A?>> f)
+        where A : class
+    {
+        return LanguageExt.Aff<Option<A>>
+            .Effect(async () => Optional(await f()));
+    }
 
-        public static Aff<Option<A>> AffOption<A>(Func<ValueTask<Nullable<A>>> f)
-            where A : struct
-        {
-            return LanguageExt.Aff<Option<A>>
-                .Effect(async () => Optional(await f()));
-        }
+    public static Aff<Option<A>> AffOption<A>(Func<ValueTask<Nullable<A>>> f)
+        where A : struct
+    {
+        return LanguageExt.Aff<Option<A>>
+            .Effect(async () => Optional(await f()));
     }
 }
--- Codehard.Functional/TaskExtensions.cs
+++ Codehard.Functional/TaskExtensions.cs
@@ -1,11 +1,10 @@
-﻿namespace Codehard.Functional
-{
-    public static class TaskExtensions
-    {
-        public static Aff<Option<A>> ToAffOption<A>(this Task<A?> ma)
-            => ma.Map(Optional).ToAff();
+﻿namespace Codehard.Functional;
 
-        public static Aff<Option<A>> ToAffOption<A>(this ValueTask<A?> ma)
-            => ma.Map(Optional).ToAff();
-    }
+public static class TaskExtensions
+{
+    public static Aff<Option<A>> ToAffOption<A>(this Task<A?> ma)
+        => ma.Map(Optional).ToAff();
+
+    public static Aff<Option<A>> ToAffOption<A>(this ValueTask<A?> ma)
+        => ma.Map(Optional).ToAff();
 }
