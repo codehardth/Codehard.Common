68f7efb48e1bd645a37c3735f6a38f4afe92829a
Chanvut Booneid <chanvut.b@codehard.co.th>
2022-10-26 16:18:13 +0700

    Fix PR comments

--- Codehard.Functional.Tests/AffGuardExtTests.cs
+++ Codehard.Functional.Tests/AffGuardExtTests.cs
@@ -1,37 +1,36 @@
 ﻿using static Codehard.Functional.Prelude;
 
-namespace Codehard.Functional.Tests
+namespace Codehard.Functional.Tests;
+
+public class AffGuardExtTests
 {
-    public class AffGuardExtTests
+    [Fact]
+    public async Task WhenGuardNoneOnSomeValue_ShouldStayOnSuccCase()
     {
-        [Fact]
-        public async Task WhenGuardNoneOnSomeValue_ShouldStayOnSuccCase()
-        {
-            // Act
-            var aff =
-                AffOption(() => ValueTask.FromResult((int?)1))
+        // Act
+        var aff =
+            AffOption(() => ValueTask.FromResult((int?)1))
                 .GuardNotNone("There is something wrong");
             
-            var fin = await aff.Run();
+        var fin = await aff.Run();
 
-            // Assert
-            var a = fin.ThrowIfFail();
+        // Assert
+        var a = fin.ThrowIfFail();
 
-            Assert.Equal(1, a);
-        }
+        Assert.Equal(1, a);
+    }
 
-        [Fact]
-        public async Task WhenGuardNoneOnNullValue_ShouldGoToFailCase()
-        {
-            // Act
-            var aff =
-                AffOption(() => ValueTask.FromResult((int?)null))
+    [Fact]
+    public async Task WhenGuardNoneOnNullValue_ShouldGoToFailCase()
+    {
+        // Act
+        var aff =
+            AffOption(() => ValueTask.FromResult((int?)null))
                 .GuardNotNone("There is no value");
 
-            var fin = await aff.Run();
+        var fin = await aff.Run();
 
-            // Assert
-            Assert.False(fin.IsSucc);
-        }
+        // Assert
+        Assert.False(fin.IsSucc);
     }
 }
--- Codehard.Functional.Tests/AffWithExtTests.cs
+++ Codehard.Functional.Tests/AffWithExtTests.cs
@@ -1,112 +1,109 @@
-using LanguageExt.Common;
+namespace Codehard.Functional.Tests;
 
-namespace Codehard.Functional.Tests
+public class AffWithExtTests
 {
-    public class AffWithExtTests
+    [Fact]
+    public async Task WhenUseWith_ShouldMatchFinResultSuccessfully()
     {
-        [Fact]
-        public async Task WhenUseWith_ShouldMatchFinResultSuccessfully()
-        {
-            // Arrage
-            var aAff = SuccessAff(1);
-            var bAff = SuccessAff("1");
+        // Arrange
+        var aAff = SuccessAff(1);
+        var bAff = SuccessAff("1");
             
-            // Act
-            var abAff = aAff.With(bAff);
-            var abFin = await abAff.Run();
+        // Act
+        var abAff = aAff.With(bAff);
+        var abFin = await abAff.Run();
 
-            // Assert
-            var ab = abFin.ThrowIfFail();
-            Assert.Equal(1, ab.Item1);
-            Assert.Equal("1", ab.Item2);
-        }
+        // Assert
+        var ab = abFin.ThrowIfFail();
+        Assert.Equal(1, ab.Item1);
+        Assert.Equal("1", ab.Item2);
+    }
 
-        [Fact]
-        public async Task WhenUseGuardWithTruePredicate_ShouldMatchFinResultSuccessfully()
-        {
-            // Arrage
-            var aAff = SuccessAff(1);
-            var bAff = SuccessAff("1");
+    [Fact]
+    public async Task WhenUseGuardWithTruePredicate_ShouldMatchFinResultSuccessfully()
+    {
+        // Arrange
+        var aAff = SuccessAff(1);
+        var bAff = SuccessAff("1");
 
-            // Act
-            var abAff = 
-                aAff
+        // Act
+        var abAff = 
+            aAff
                 .With(bAff)
                 .Guard(
                     ab => ab.Item1 == 1 && ab.Item2 == "1",
                     Error.New(1, "Wrong value!"));
 
-            var abFin = await abAff.Run();
+        var abFin = await abAff.Run();
 
-            // Assert
-            var ab = abFin.ThrowIfFail();
-            Assert.Equal(1, ab.Item1);
-            Assert.Equal("1", ab.Item2);
-        }
+        // Assert
+        var ab = abFin.ThrowIfFail();
+        Assert.Equal(1, ab.Item1);
+        Assert.Equal("1", ab.Item2);
+    }
 
-        [Fact]
-        public async Task WhenUseGuardMultiArgsWithTruePredicate_ShouldMatchFinResultSuccessfully()
-        {
-            // Arrage
-            var aAff = SuccessAff(1);
-            var bAff = SuccessAff("1");
+    [Fact]
+    public async Task WhenUseGuardMultiArgsWithTruePredicate_ShouldMatchFinResultSuccessfully()
+    {
+        // Arrange
+        var aAff = SuccessAff(1);
+        var bAff = SuccessAff("1");
 
-            // Act
-            var abAff =
-                aAff
+        // Act
+        var abAff =
+            aAff
                 .With(bAff)
                 .Guard(
                     (a, b) => a == 1 && b == "1",
                     Error.New(1, "Wrong value!"));
 
-            var abFin = await abAff.Run();
+        var abFin = await abAff.Run();
 
-            // Assert
-            var ab = abFin.ThrowIfFail();
-            Assert.Equal(1, ab.Item1);
-            Assert.Equal("1", ab.Item2);
-        }
+        // Assert
+        var ab = abFin.ThrowIfFail();
+        Assert.Equal(1, ab.Item1);
+        Assert.Equal("1", ab.Item2);
+    }
 
-        [Fact]
-        public async Task WhenUseGuardWithFalsePredicate_ShouldMatchFinResultFail()
-        {
-            // Arrage
-            var aAff = SuccessAff(1);
-            var bAff = SuccessAff("1");
+    [Fact]
+    public async Task WhenUseGuardWithFalsePredicate_ShouldMatchFinResultFail()
+    {
+        // Arrange
+        var aAff = SuccessAff(1);
+        var bAff = SuccessAff("1");
 
-            // Act
-            var abAff =
-                aAff
+        // Act
+        var abAff =
+            aAff
                 .With(bAff)
                 .Guard(
                     ab => ab.Item1 == 2 && ab.Item2 == "2",
                     Error.New(1, "Wrong value!"));
 
-            var abFin = await abAff.Run();
+        var abFin = await abAff.Run();
 
-            // Assert
-            Assert.True(abFin.IsFail);
-        }
+        // Assert
+        Assert.True(abFin.IsFail);
+    }
 
-        [Fact]
-        public async Task WhenUseGuardDeconstructTupleWithFalsePredicate_ShouldMatchFinResultFail()
-        {
-            // Arrage
-            var aAff = SuccessAff(1);
-            var bAff = SuccessAff("1");
+    [Fact]
+    public async Task WhenUseGuardDeconstructTupleWithFalsePredicate_ShouldMatchFinResultFail()
+    {
+        // Arrange
+        var aAff = SuccessAff(1);
+        var bAff = SuccessAff("1");
 
-            // Act
-            var abAff =
-                aAff
+        // Act
+        var abAff =
+            aAff
                 .With(bAff)
                 .Guard(
                     (a, b) => a == 2 && b == "2",
                     Error.New(1, "Wrong value!"));
 
-            var abFin = await abAff.Run();
+        var abFin = await abAff.Run();
 
-            // Assert
-            Assert.True(abFin.IsFail);
-        }
+        // Assert
+        Assert.True(abFin.IsFail);
     }
 }
