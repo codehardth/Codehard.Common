93f3c26dd3c3d213509b00ab372ea5a27899749a
Deszolate_C <deszolate@icloud.com>
2023-01-31 17:47:03 +0700

    Add entity with lazy loader

---
+++ src/Codehard.Common/Codehard.Common.DomainModel/Entity.cs
@@ -0,0 +1,23 @@
+using Codehard.Common.DomainModel.Extensions;
+
+namespace Codehard.Common.DomainModel;
+
+public abstract record Entity<T>
+    : IEntity<T> where T : struct
+{
+    private readonly Action<object, string> lazyLoader;
+
+    protected Entity(Action<object, string> lazyLoader)
+    {
+        this.lazyLoader = lazyLoader;
+    }
+
+    public abstract T Id { get; }
+
+    protected TRelated LoadNavigationProperty<TRelated>(TRelated refInstance)
+        where TRelated : class
+        => this.lazyLoader.Load(this, ref refInstance);
+
+    protected IReadOnlyCollection<TRelated> LoadNavigationPropertyCollection<TRelated>(List<TRelated> collection)
+        => this.lazyLoader.Load(this, ref collection);
+}
