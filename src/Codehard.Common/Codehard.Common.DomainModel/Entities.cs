52f2346881f2a1575f8430594ffd3546fd87064d
Deszolate_C <deszolate@icloud.com>
2023-02-02 11:52:42 +0700

    Make entity with key implemented abstract

--- src/Codehard.Common/Codehard.Common.DomainModel/Entities.cs
+++ src/Codehard.Common/Codehard.Common.DomainModel/Entities.cs
@@ -1,21 +1,21 @@
 namespace Codehard.Common.DomainModel;
 
-public class EntityWithIntegerKey : Entity<IntegerKey>
+public abstract class EntityWithIntegerKey : Entity<IntegerKey>
 {
     public override IntegerKey Id { get; protected init; }
 }
 
-public class EntityWithLongKey : Entity<LongKey>
+public abstract class EntityWithLongKey : Entity<LongKey>
 {
     public override LongKey Id { get; protected init; }
 }
 
-public class EntityWithStringKey : Entity<StringKey>
+public abstract class EntityWithStringKey : Entity<StringKey>
 {
     public override StringKey Id { get; protected init; }
 }
 
-public class EntityWithGuidKey : Entity<GuidKey>
+public abstract class EntityWithGuidKey : Entity<GuidKey>
 {
     public override GuidKey Id { get; protected init; }
 }
