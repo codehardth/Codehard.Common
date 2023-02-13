a66422c40d575fa32b434ab5c0f2f3598f2a1f3d
Desz01ate <deszolate@icloud.com>
2023-02-13 11:31:22 +0700

    Add constructors for entity

--- src/Codehard.Common/Codehard.Common.DomainModel/Entities.cs
+++ src/Codehard.Common/Codehard.Common.DomainModel/Entities.cs
@@ -2,20 +2,56 @@ namespace Codehard.Common.DomainModel;
 
 public abstract class EntityWithIntegerKey : Entity<IntegerKey>
 {
+    protected EntityWithIntegerKey()
+    {
+    }
+
+    protected EntityWithIntegerKey(Action<object, string> lazyLoader)
+        : base(lazyLoader)
+    {
+    }
+
     public override IntegerKey Id { get; protected init; }
 }
 
 public abstract class EntityWithLongKey : Entity<LongKey>
 {
+    protected EntityWithLongKey()
+    {
+    }
+
+    protected EntityWithLongKey(Action<object, string> lazyLoader)
+        : base(lazyLoader)
+    {
+    }
+
     public override LongKey Id { get; protected init; }
 }
 
 public abstract class EntityWithStringKey : Entity<StringKey>
 {
+    protected EntityWithStringKey()
+    {
+    }
+
+    protected EntityWithStringKey(Action<object, string> lazyLoader)
+        : base(lazyLoader)
+    {
+    }
+
     public override StringKey Id { get; protected init; }
 }
 
 public abstract class EntityWithGuidKey : Entity<GuidKey>
 {
+    protected EntityWithGuidKey()
+    {
+    }
+
+    protected EntityWithGuidKey(Action<object, string> lazyLoader)
+        : base(lazyLoader)
+    {
+    }
+
     public override GuidKey Id { get; protected init; }
 }
