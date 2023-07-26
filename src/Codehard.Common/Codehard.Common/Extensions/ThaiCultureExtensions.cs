8a1b3f3d2d1f2ea05a96b2af17408e04ef552d8d
Rung <rungwiroon@gmail.com>
2023-07-26 15:01:09 +0700

    Add more common functions

---
+++ src/Codehard.Common/Codehard.Common/Extensions/ThaiCultureExtensions.cs
@@ -0,0 +1,83 @@
+using System.Globalization;
+
+namespace Codehard.Common.Extensions;
+
+public static class ThaiCultureExtensions
+{
+    public static readonly CultureInfo ThaiCultureInfo = CultureInfo.GetCultureInfo("th-TH");
+    
+    /// <summary>
+    /// This method converts a string to Thai number.
+    /// </summary>
+    /// <param name="src">The string to convert</param>
+    /// <returns>The converted string</returns>
+    public static string ToThaiNumber(this string src)
+    {
+        if (src.Length == 0)
+        {
+            return src;
+        }
+
+        // Up to 100kB
+        const uint limit = 102_400;
+        const int charSize = sizeof(char);
+        const int startRange = 48;
+        const int endRange = 57;
+
+        return src.Length * charSize > limit
+            ? ConvertToThaiNumberOnHeap(src)
+            : ConvertToThaiNumberOnStack(src);
+
+        static string ConvertToThaiNumberOnStack(string src)
+        {
+            Span<char> chars = stackalloc char[src.Length];
+
+            for (var idx = 0; idx < src.Length; idx++)
+            {
+                chars[idx] = Convert(src[idx]);
+            }
+
+            return new string(chars);
+        }
+
+        static string ConvertToThaiNumberOnHeap(string src)
+        {
+            var chars = src.ToCharArray();
+
+            for (var idx = 0; idx < src.Length; idx++)
+            {
+                chars[idx] = Convert(src[idx]);
+            }
+
+            return new string(chars);
+        }
+
+        static char Convert(char c)
+        {
+            var charCode = (int)c;
+
+            return charCode is < startRange or > endRange ? c : (char)(charCode + 3616);
+        }
+    }
+    
+    public static string ToThaiDateString(
+        this DateTime dateTime,
+        string format = "dd/MM/yyyy",
+        bool thaiNumber = false)
+    {
+        var dateText = dateTime.ToString(
+            format,
+            ThaiCultureInfo);
+
+        return thaiNumber ? dateText.ToThaiNumber() : dateText;
+    }
+
+    public static string ToThaiDateString(
+        this DateTimeOffset dateTimeOffset,
+        string format = "dd/MM/yyyy")
+    {
+        return dateTimeOffset.ToString(
+            format,
+            ThaiCultureInfo);
+    }
+}
