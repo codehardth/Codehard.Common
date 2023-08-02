4e6b6cf806cf4b40fe72dbb34104329fda439342
Desz01ate <deszolate@icloud.com>
2023-08-02 16:49:38 +0700

    Adds string extensions

---
+++ src/Codehard.Functional/Codehard.Functional/StringExtensions.cs
@@ -0,0 +1,23 @@
+using System.Diagnostics.Contracts;
+
+namespace Codehard.Functional;
+
+/// <summary>
+/// Provides extension methods for a string.
+/// </summary>
+public static class StringExtensions
+{
+    /// <summary>
+    /// Returns the original string if it's not null or empty, otherwise returns the alternative value.
+    /// </summary>
+    [Pure]
+    public static string IfNullOrEmpty(this string s, string alternative)
+        => string.IsNullOrEmpty(s) ? alternative : s;
+
+    /// <summary>
+    /// Returns the original string if it's not null or whitespace, otherwise returns the alternative value.
+    /// </summary>
+    [Pure]
+    public static string IfNullOrWhitespace(this string s, string alternative)
+        => string.IsNullOrWhiteSpace(s) ? alternative : s;
+}
