5520a5a3edb5ac00c62545820d108548c63348a9
Desz01ate <deszolate@icloud.com>
2023-08-09 15:57:39 +0700

    Convert Functional.AspNetCore code to source-generated version

--- src/Codehard.Functional/Codehard.Functional.AspNetCore/Globals.cs
+++
@@ -1,6 +0,0 @@
-global using System.Net;
-global using LanguageExt;
-global using LanguageExt.Common;
-global using Microsoft.AspNetCore.Mvc;
-global using Microsoft.Extensions.Logging;
-global using static LanguageExt.Prelude;
---
+++ src/Codehard.Functional/Codehard.Functional.AspNetCore/Shared/Constants.cs
@@ -0,0 +1,6 @@
+namespace Codehard.Functional.AspNetCore.Shared;
+
+internal static class Constants
+{
+    public static string Namespace = "Codehard.Functional.AspNetCore";
+}
