c4f02cd33b3ee39a93b256053a209c2831b62dbf
gfhdlcod02 <gfhdlcod02@gmail.com>
2023-11-14 14:07:21 +0700

    Add Reed File Extension

---
+++ src/Codehard.Common.AspNetCore.Test/ReadFileExtensionTest.cs
@@ -0,0 +1,33 @@
+﻿using Codehard.Common.AspNetCore.Extensions;
+using Microsoft.AspNetCore.Http;
+using Moq;
+
+namespace Codehard.Common.AspNetCore.Test;
+
+public class ReadFileExtensionTest
+{
+    [Fact]
+    public async Task ReadFileAsync_ShouldReturnByteArray()
+    {
+        // Arrange
+        var formFile = new Mock<IFormFile>();
+        var cancellationToken = new CancellationToken();
+        var content = "Hello World from a Fake File";
+        var ms = new MemoryStream();
+        var writer = new StreamWriter(ms);
+        await writer.WriteAsync(content);
+        await writer.FlushAsync();
+        ms.Position = 0;
+        formFile.Setup(file => file.CopyToAsync(It.IsAny<Stream>(), cancellationToken))
+                .Callback<Stream, CancellationToken>((stream, token) => ms.CopyTo(stream))
+                .Returns(Task.CompletedTask);
+
+        // Act
+        var result = await formFile.Object.ReadFileAsync(cancellationToken);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(ms.Length, result.Length);
+        Assert.IsType<byte[]>(result);
+    }
+}
---
+++ src/Codehard.Common/Codehard.Common.AspNetCore/Extensions/ReadFileExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Codehard.Common.AspNetCore.Extensions;
+
+/// <summary>
+/// Read file extension.
+/// </summary>
+public static class ReadFileExtension
+{
+    /// <summary>
+    /// Read file as byte array.
+    /// </summary>
+    /// <param name="formFile"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public static async Task<byte[]> ReadFileAsync(this IFormFile formFile, CancellationToken cancellationToken)
+    {
+        await using var memoryStream = new MemoryStream();
+        await formFile.CopyToAsync(memoryStream, cancellationToken);
+        return memoryStream.ToArray();
+    }
+}
