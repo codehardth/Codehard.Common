2cb4d1bf667c239aa81def73ae03e209ec6c07b9
rung <rungwiroon@gmail.com>
2024-10-24 00:37:08 +0700

    Experiment with Eff runtime

--- src/Codehard.Functional/Codehard.Functional.EntityFramework.Tests/QueryableExtensionsTests.cs
+++ src/Codehard.Functional/Codehard.Functional.EntityFramework.Tests/QueryableExtensionsTests.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 using Codehard.Functional.EntityFramework.Tests.Entities;
 using Codehard.Infrastructure.EntityFramework.Extensions;
+using static Codehard.Functional.EntityFramework.Extensions.QueryablePrelude;
 
 namespace Codehard.Functional.EntityFramework.Tests;
 
@@ -18,7 +19,7 @@ public class QueryableExtensionsTests
     }
     
     [Fact]
-    public async Task ToListEffRt_ShouldReturnList()
+    public async Task ToListEff_ShouldReturnList()
     {
         // Arrange
         var assembly = Assembly.GetExecutingAssembly();
@@ -29,6 +30,7 @@ public class QueryableExtensionsTests
         await using var context = new TestDbContext(
             options,
             builder => builder.ApplyConfigurationsFromAssemblyFor<TestDbContext>(assembly));
+        
         await context.Database.EnsureCreatedAsync();
         
         var entityId = Guid.NewGuid();
@@ -52,6 +54,46 @@ public class QueryableExtensionsTests
         var list = await result;
         
         var resultEntity = list.ThrowIfFail();
+        
+        Assert.NotNull(resultEntity);
+        Assert.Single(resultEntity);
+    }
+    
+    [Fact]
+    public async Task FilterRtAndToListEff_ShouldReturnList()
+    {
+        // Arrange
+        var assembly = Assembly.GetExecutingAssembly();
+        var options = new DbContextOptionsBuilder<TestDbContext>()
+            .UseSqlite(CreateInMemoryDatabase())
+            .Options;
+        
+        await using var context = new TestDbContext(
+            options,
+            builder => builder.ApplyConfigurationsFromAssemblyFor<TestDbContext>(assembly));
+        
+        await context.Database.EnsureCreatedAsync();
+        
+        var entityId = Guid.NewGuid();
+        var entity = new EntityA
+        {
+            Id = entityId,
+        };
+        
+        context.As.Add(entity);
+        await context.SaveChangesAsync();
+
+        // Act
+        var workflow =
+            from @as in From<EntityA>()
+            from list in
+                @as.Where(a => a.Id == entityId)
+                   .ToListEff()
+            select list;
+
+        // Assert
+        var listResult = await workflow.RunAsync(context);
+        var resultEntity = listResult.ThrowIfFail();
         
         Assert.NotNull(resultEntity);
         Assert.Single(resultEntity);
