ee74485c1250b3903b9aeec6a925c8937db5c37c
Rung <rungwiroon@gmail.com>
2024-10-24 13:50:50 +0700

    Add ErrorWrapperActionResultFilter

--- src/Codehard.Functional/Codehard.Functional.AspNetCore.Tests.Api/Program.cs
+++ src/Codehard.Functional/Codehard.Functional.AspNetCore.Tests.Api/Program.cs
@@ -1,8 +1,15 @@
+using Codehard.Functional.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddMvc(options =>
+{
+    options.Filters.Add<ErrorWrapperActionResultLoggingFilter>();
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,7 +23,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
---
+++ src/Codehard.Functional/Codehard.Functional.AspNetCore/ErrorWrapperActionResultLoggingFilter.cs
@@ -0,0 +1,96 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace Codehard.Functional.AspNetCore;
+
+/// <summary>
+/// An action filter that logs errors occurring during the execution of an action.
+/// </summary>
+public class ErrorWrapperActionResultLoggingFilter : IAsyncActionFilter
+{
+    private readonly ILogger<ErrorWrapperActionResultLoggingFilter> logger;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ErrorWrapperActionResultLoggingFilter"/> class.
+    /// </summary>
+    /// <param name="logger">The logger to log error information.</param>
+    public ErrorWrapperActionResultLoggingFilter(ILogger<ErrorWrapperActionResultLoggingFilter> logger)
+    {
+        this.logger = logger;
+    }
+    
+    /// <summary>
+    /// Executes the action and logs any errors that occur during the execution.
+    /// </summary>
+    /// <param name="context">The context in which the action is executed.</param>
+    /// <param name="next">The delegate to execute the next action filter or action.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        var actionExecutedContext = await next();
+        
+        await OnActionExecutedAsync(actionExecutedContext);
+    }
+    
+    private Task OnActionExecutedAsync(ActionExecutedContext context)
+    {
+        var traceId = context.HttpContext.TraceIdentifier;
+
+        switch (context.Result)
+        {
+            case ErrorWrapperActionResult ewar:
+                LogHttpResultError(ewar.Error);
+                break;
+            case IStatusCodeActionResult statusCodeResult:
+            {
+                if(statusCodeResult.StatusCode == StatusCodes.Status500InternalServerError)
+                {
+                    LogError(context.Exception);
+                }
+
+                break;
+            }
+        }
+
+        return Task.CompletedTask;
+        
+        void LogHttpResultError(HttpResultError error)
+        {
+            this.logger.LogError(
+                message: "TraceId: {TraceId}, {Path}, {Query}, {Method}, {ResponseStatus}, {ErrorCode}",
+                traceId,
+                context.HttpContext.Request.Path,
+                context.HttpContext.Request.Query,
+                context.HttpContext.Request.Method,
+                error.StatusCode,
+                error.ErrorCode.IfNoneUnsafe(default(string)));
+
+            LogErrorOpt(error.Inner);
+        }
+        
+        void LogErrorOpt(Option<Error> errorOpt)
+        {
+            errorOpt.Iter(
+                Some: error =>
+                {
+                    LogError(
+                        exception: error.Exception.IfNoneUnsafe(default(Exception)));
+                    
+                    LogErrorOpt(error.Inner);
+                });
+        }
+
+        void LogError(Exception? exception)
+        {
+            this.logger.LogError(
+                exception: exception,
+                message: "TraceId: {TraceId}, {Path}, {Query}, {Method}, {ResponseStatus}",
+                traceId,
+                context.HttpContext.Request.Path,
+                context.HttpContext.Request.Query,
+                context.HttpContext.Request.Method,
+                HttpStatusCode.InternalServerError);
+        }
+    }
+}
